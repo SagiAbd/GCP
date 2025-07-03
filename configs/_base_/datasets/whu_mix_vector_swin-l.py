45945edb04136caa54102da3b576881c783c9689
SagiAbd <sagibdashim@gmail.com>
2025-07-03 10:29:57 +0500

    changed batch size and num workers

--- configs/_base_/datasets/whu_mix_vector_swin-l.py
+++ configs/_base_/datasets/whu_mix_vector_swin-l.py
@@ -64,7 +64,7 @@ test_pipeline = [
 
 train_dataloader = dict(
     batch_size=2,
-    num_workers=2,
+    num_workers=4,
     # persistent_workers=True,
     persistent_workers=False,
     sampler=dict(type='DefaultSampler', shuffle=True),
@@ -81,7 +81,7 @@ train_dataloader = dict(
 
 val_dataloader = dict(
     batch_size=2,
-    num_workers=2,
+    num_workers=4,
     # persistent_workers=True,
     persistent_workers=False,
     drop_last=False,
@@ -100,8 +100,8 @@ val_dataloader = dict(
     )
 )
 test_dataloader = dict(
-    batch_size=1,
-    num_workers=1,
+    batch_size=2,
+    num_workers=4,
     # persistent_workers=True,
     persistent_workers=False,
     drop_last=False,
