8bf5aeeed78f72008a1d571b12f5d7770534844e
SagiAbd <sagibdashim@gmail.com>
2025-07-03 15:11:06 +0500

    ex[

--- configs/_base_/datasets/whu_mix_vector_swin-l.py
+++ configs/_base_/datasets/whu_mix_vector_swin-l.py
@@ -74,7 +74,7 @@ train_dataloader = dict(
         data_root=data_root,
         pipeline=train_pipeline,
         backend_args=backend_args,
-        ann_file='fgfgdsa/train/train.json',
+        ann_file='gggggg/train.json',
         data_prefix=dict(img='gggggg/image'),
     )
 )
