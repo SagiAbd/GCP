67bfcde55b7e768c889486913d8d261100c3fc0e
SagiAbd <sagibdashim@gmail.com>
2025-07-08 09:27:06 +0500

    Temp change

--- configs/_base_/datasets/whu_mix_vector_swin-l.py
+++ configs/_base_/datasets/whu_mix_vector_swin-l.py
@@ -74,8 +74,8 @@ train_dataloader = dict(
         data_root=data_root,
         pipeline=train_pipeline,
         backend_args=backend_args,
-        ann_file='gggggg/train.json',
-        data_prefix=dict(img='gggggg/image'),
+        ann_file='whu-mix-vector-dataset-val/val.json',
+        data_prefix=dict(img='whu-mix-vector-dataset-val/image'),
     )
 )
 
